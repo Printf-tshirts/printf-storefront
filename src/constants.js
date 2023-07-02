a22e378bbfc4338de5c4604c26090c5e826ffc76
Jai Malani <malanijai05@gmail.com>
2023-07-03 00:01:57 +0530

    backend url updated

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:5000/api";
-export const BACKEND_URL = "http://3.7.65.142:5000/api";
+export const BACKEND_URL = "http://server.printcoder.com:5000/api";
